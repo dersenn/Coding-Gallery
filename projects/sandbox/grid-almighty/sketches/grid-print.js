293253847fc4c73df4d565f08891451b09086765
Christoph Senn <67318888+dersenn@users.noreply.github.com>
2026-04-16 21:05:58 +0200

    goodie.

--- projects/sandbox/grid-almighty/sketches/grid-print.js
+++ projects/sandbox/grid-almighty/sketches/grid-print.js
@@ -1,22 +1,87 @@
+import { Grid, GridCell } from '~/types/project'
 import { shortcuts } from '~/utils/shortcuts'
+import { lightTheme } from '~/utils/theme'
+
+
+
+class MyGrid extends Grid {
+  createCell(config) {
+    return new MyCell(config)
+  }
+}
+
+class MyCell extends GridCell {
+  constructor(config) {
+    super(config)
+  }
+
+  draw(canvas, colors, maxLevel, spacing) {
+    const { v, curve, rnd, rndInt, map, norm } = shortcuts(this.grid.utils)
+
+    const fill = (this.col + this.row) % 2 === 0 ? colors[0] : colors[1]
+
+    canvas.rect(v(this.x, this.y), this.width, this.height, fill, 'transparent', 0)
+  }
+}
+
 
 
 export function draw(context) {
   const { canvas, utils, controls: c } = context
-  const { v, rnd, curve } = shortcuts(utils)
-
+  const { v, rnd, curve, pickMany } = shortcuts(utils)
   if (!canvas) return
-  const { mm, pt, trimWidth, trimHeight, drawTrimBox } = canvas.print
 
-  canvas.background('#fff')
+  const { mm, drawTrimBox } = canvas.print
 
-  canvas.halftone(
-    v(mm(10), mm(10)),
-    mm(50), mm(297-20),
-    '#000',
-    (nx, ny) => 1 - curve.easeIn(ny),
-    { spacing: mm(.3), rng: rnd }
-  )
+  const border = {
+    top: mm(9),
+    right: mm(9),
+    bottom: mm(15),
+    left: mm(9),
+  }
+
+  const maxLevel = 1
+
+  const colors = pickMany(lightTheme.palette, maxLevel + 1)
+
+  const grid = new MyGrid({
+    cols: 3,
+    rows: 5,
+    width: canvas.w - border.left - border.right,
+    height: canvas.h - border.top - border.bottom,
+    x: border.left,
+    y: border.top,
+    utils,
+  })
+
+  const terminals = grid.subdivide({
+    maxLevel: 1,
+    rule: (cell) => {
+      if ((cell.col + cell.row) % 2 === 0) return {cols: 2, rows: 2}
+      return { cols: 4, rows: 4 }
+      // return { cols: 1, rows: 1 }
+    }
+  })
+
+
+
+  canvas.background(lightTheme.background)
+
+
+  terminals.forEach(cell => {
+    cell.draw(canvas, colors, maxLevel, 1)
+  })
+
+ 
+  
+
+  // canvas.halftone(
+  //   v(mm(10), mm(10)),
+  //   mm(50), mm(297-20),
+  //   '#000',
+  //   (nx, ny) => 1 - curve.easeIn(ny),
+  //   { spacing: mm(.3), rng: rnd }
+  // )
 
   // drawTrimBox(canvas.ctx)
 }
